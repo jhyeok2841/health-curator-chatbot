e5e08cd7eb0fa877c25dec128263dbaa8c38f306
jhyeok2841 <156050708+jhyeok2841@users.noreply.github.com>
2025-05-01 11:16:38 +0900

    Update st_function.py

--- st_function.py
+++ st_function.py
@@ -3,40 +3,12 @@ import streamlit as st
 from langchain_core.messages.chat import ChatMessage
 # from rag_bk.modules.handler import format_search_result
 
-
 # 클레스 타입 지정 -> web_search시 답변을 이쁘게 만들기 위함.
 @dataclass
 class ChatMessageWithType:
     chat_message: ChatMessage
     msg_type: str
     tool_name: str
-
-
-# 이전 대화를 출력
-# def print_messages():
-#     for message in st.session_state["messages"]:
-
-#         # 그냥 일반 대화라면
-#         if message.msg_type == "text":
-#             st.chat_message(message.chat_message.role).write(
-#                 message.chat_message.content
-#             )
-#         # tool을 사용한 경우
-#         elif message.msg_type == "tool_result":
-#             # tool이 web_search 이면 아래 format으로 답변
-#             if message.tool_name == "web_search":
-#                 with st.expander(f"✅ {message.tool_name}"): 
-#                     st.markdown(message.chat_message.content)
-#             # tool이 pdf_search 이면 아래 format으로 답변
-#             elif message.tool_name == "pdf_search":
-#                 with st.expander(f"✅ {message.tool_name}"):
-#                     st.chat_message(message.chat_message.role).write(
-#                         message.chat_message.content
-#                     )
-#             # else:
-#             #     st.chat_message(message.chat_message.role).write(
-#             #         message.chat_message.content
-#             #     )
 
 # 이전 대화를 출력
 def print_messages():
